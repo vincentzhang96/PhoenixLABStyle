88e9d99ad7dbf6825f1aecaf5fb9c041c58cceed
Vincent Zhang <vzhang@purdue.edu>
2016-05-01 22:20:45 -0400

    Set up project

---
+++ gulp.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
+});
